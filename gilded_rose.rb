28f2b3a4fbe3dbfb09fb5d804e55326473266b33
Lewis Reid <lreidwilliams@gmail.com>
2023-11-13 23:05:35 +1000

    Pulled aged brie logic out of the nested conditionals and into its own block moving towards a flatter structure

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -2,45 +2,43 @@ def update_quality(items)
   items.each do |item|
     next if item.name == 'Sulfuras, Hand of Ragnaros'
 
-    if item.name != 'Aged Brie' && item.name != 'Backstage passes to a TAFKAL80ETC concert'
+    item.sell_in -= 1
+
+    if item.name == 'Aged Brie'
+      item.quality += 1 if item.quality < 50
+      item.quality += 1 if item.quality < 50 && item.sell_in < 0
+      next
+    end
+
+    if item.name != 'Backstage passes to a TAFKAL80ETC concert'
       if item.quality > 0
         item.quality -= 1
       end
     else
       if item.quality < 50
         item.quality += 1
-        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-          if item.sell_in <= 10
-            if item.quality < 50
-              item.quality += 1
-            end
+        if item.sell_in < 10
+          if item.quality < 50
+            item.quality += 1
           end
-          if item.sell_in <= 5
-            if item.quality < 50
-              item.quality += 1
-            end
+        end
+        if item.sell_in < 5
+          if item.quality < 50
+            item.quality += 1
           end
         end
       end
     end
 
-    if item.sell_in <= 0
-      if item.name != "Aged Brie"
-        if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-          if item.quality > 0
-            item.quality -= 1
-          end
-        else
-          item.quality = item.quality - item.quality
+    if item.sell_in < 0
+      if item.name != 'Backstage passes to a TAFKAL80ETC concert'
+        if item.quality > 0
+          item.quality -= 1
         end
       else
-        if item.quality < 50
-          item.quality += 1
-        end
+        item.quality = item.quality - item.quality
       end
     end
-
-    item.sell_in -= 1
   end
 end
 
